cd5035aa6a80c28403bc6301f7fe84b1fa431c00
Arasy Hemasthiar <arasyhemas117@gmail.com>
2023-09-14 16:05:16 +0700

    Upload avatar endpoint

--- user/service.go
+++ user/service.go
@@ -10,6 +10,7 @@ type Service interface {
 	RegisterUser(input RegisterUserInput) (User, error)
 	Login(input LoginInput) (User, error)
 	IsEmailAvailable(input CheckEmailInput) (bool, error)
+	SaveAvatar(ID int, fileLocation string) (User, error)
 }
 
 type service struct {
@@ -70,6 +71,25 @@ func (s *service) IsEmailAvailable(input CheckEmailInput) (bool, error) {
 		return true, nil
 	}
 	return false, nil
+}
+
+func (s *service) SaveAvatar(ID int, fileLocation string) (User, error) {
+	//1. dapatkan user berdasarkan id
+	//2. update atribut avatar file name
+	//3. simpan perubahan avatar file name
+	user, err := s.repository.FindByID(ID)
+	if err != nil {
+		return user, err
+	}
+
+	user.Avatar_file_name = fileLocation
+	updatedUser, err := s.repository.Update(user)
+	if err != nil {
+		return updatedUser, err
+	}
+
+	return updatedUser, nil
+
 }
 
 //MAPPING STRUCT INPUT KE STRUCT USER
